08fcf0047072109ae970ba2db1b99327db613ba7
matiasGutierrez <m.ignacio.guti@gmail.com>
2023-05-08 20:59:25 -0400

    Esconder credenciales

--- server/database/conexionBaseDeDatos.js
+++ server/database/conexionBaseDeDatos.js
@@ -1,5 +1,5 @@
 const DATABASE_URL =
-  'postgresql://matias:4A86UUsO7K3jN3AypIMvWw@mrkt-latam-10806.7tt.cockroachlabs.cloud:26257/marketplace?sslmode=verify-full';
+  'postgresql://:@mrkt-latam-10806.7tt.cockroachlabs.cloud:26257/marketplace?sslmode=verify-full';
 
 const { Client } = require('pg');
 
