646be2a79d9b30fc291f9c02ca674675c1b0305a
Felipe Fuentes <felipe.c.fuentes.g@gmail.com>
2023-05-19 00:27:25 -0400

    Se agrega formulario al login sin logica

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ function NavBarHome() {
   return (
     <Navbar bg="light" expand="lg">
       <Container fluid>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+        <Navbar.Brand href="#">MarketPlace</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
